bee6755811bee7e4712da21d0b165e0da50c4625
raphaelc484 <raphaelc484@hotmail.com>
2025-02-26 15:00:33 -0300

    Erro na conexão com google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,7 +6,14 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email openid https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
 }
+
 export default NextAuth(authOptions)
