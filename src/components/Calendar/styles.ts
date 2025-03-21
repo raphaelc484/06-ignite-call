b0cd1b277f4b2f04563e3aa746518469f2f7ce6c
raphaelc484 <raphaelc484@hotmail.com>
2025-03-21 14:15:41 -0300

    Calendário e disponibilidade - Estrutura visual do calendário

---
+++ src/components/Calendar/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from '@ignite-ui/react'
+
+export const CalendarContainer = styled('div', {})
+
+export const CalendarHeader = styled('div', {})
+
+export const CalendarTitle = styled('div', {})
+
+export const CalendarActions = styled('div', {})
+
+export const CalendarBody = styled('table', {})
+
+export const CalendarDay = styled('button', {})
